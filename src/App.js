dda6e8783a749a7f41e6b95b40406e07546e5972
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-18 13:02:20 -0700

    Added nav links, change function, and sent props to form

--- src/App.js
+++ src/App.js
@@ -4,57 +4,63 @@ import axios from 'axios';
 import { Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Plant_List from "./components/Plants_List";
+import Plant_Form from "./components/Plant_Form";
 
+const initialFormValues = {
+    nickname: '',
+    species: '',
+    h2oFrequency: '',
+    image:'',
+    user_id:''
+  }
 
 function App() {
+
+    const [formValues, setFormValues]=useState(initialFormValues)
 
     useEffect(() => {
         axios.get("https://watermyplantsbwweb46.herokuapp.com/api")
             .then(res => {
-                console.log(res.data)
+                // console.log(res.data)
             })
             .catch(err => {
                 console.error(err)
             })
-
-
     }, [])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const change= (evt) =>{
+        if(evt.target.type === 'checkbox' ? setFormValues({ ...formValues, [evt.target.name]: evt.target.checked }) :  setFormValues({ ...formValues, [evt.target.name]: evt.target.value }));
+        // validate(evt.target.name, evt.target.value);
+      }
 
 
     return (
      <>
     <div className="App">
-        Water My Plants
+        <header>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/Plant_Form">Add New Plant</Link>
+                <Link to="/Plants_List">My Plants</Link>
+            </nav>
+            <h1>Water My Plants</h1>
+        </header>   
+        <div className="home-component">
+            <Route path="/">
+                <Home />
+            </Route>
+        </div>
+        <div>
+            <Route path="plant-list" >
+                <Plant_List />
+            </Route>
+        </div>
+        <div>
+            <Route path="/Plant_Form" >
+                <Plant_Form change={change} formValues={formValues}/>
+            </Route>
+        </div>
     </div>
-    <div className="home-component">
-        <Route path="/">
-            <Home />
-        </Route>
-    </div>
-    <div>
-        <Route path="plant-list" >
-            <Plant_List />
-        </Route>
-    </div>
-
      </>
 
   )
--- src/components/Plant_Form.js
+++ src/components/Plant_Form.js
@@ -2,55 +2,60 @@ import React from 'react'
 
 export default function Plant_Form(props){
     const{
-        values,
-        // errors
+        formValues,
+        change,
+        errors
     } = props
+    console.log(formValues.nickname)
 
     return(
-        <form className='form-container' onSubmit={onSubmit}>
+        <form className='form-container' >
             <div className='form-inputs submit'>
                 <h2>Add New Plant</h2>
                 <button>submit</button>
-                {/* <div className='errors'>
-                    <div>{errors.name}</div>
+                <div className='errors'>
+                    {/* <div>{errors.name}</div>
                     <div>{errors.species}</div>
                     <div>{errors.h2oFrequency}</div>
-                    <div>{errors.image}</div>
-                </div> */}
+                    <div>{errors.image}</div> */}
+                </div>
             </div>
 
             <div className='form-group inputs'>
                 <h3>User Information</h3>
                 <label>Nickname &nbsp;
                     <input
-                        value={values.name}
-                        onChange={onChange}
+                        value={formValues.nickname}
+                        onChange={change}
                         name='nickname'
                         type='text'
+                        placeholder='Nickname'
                     />
                 </label>
                 <label>Species &nbsp;
                     <input
-                        value={values.species}
-                        onChange={onChange}
+                        value={formValues.species}
+                        onChange={change}
                         name='species'
                         type='text'
+                        placeholder='Species'
                     />
                 </label>
                 <label>How often do you water this plant? &nbsp;
                     <input
-                        value={values.h2oFrequency}
-                        onChange={onChange}
+                        value={formValues.h2oFrequency}
+                        onChange={change}
                         name='h2oFrequency'
                         type='text'
+                        placeholder='Water schedule?'
                     />
                 </label>
                 <label>Image: &nbsp;
                     <input
                         type="image"
-                        onChange={onChange}
+                        onChange={change}
                         name='image'
-                        value={values.image}
+                        value={formValues.image}
                     />
                 </label>
             </div>

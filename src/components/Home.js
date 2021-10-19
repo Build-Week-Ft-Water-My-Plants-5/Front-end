abd20186bddbc16262fa819fd8a1f5281a56e68f
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-19 16:52:56 -0700

    Changed h1 to h2 in home.js so there is only one h1 component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
 
     return (
         <div className='home-container'>
-            <h1>Welcome to the Watering Hole!</h1>
+            <h2>Welcome to the Watering Hole!</h2>
             {/* <img src="https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="flowering plant"/> */}
         </div>
     )

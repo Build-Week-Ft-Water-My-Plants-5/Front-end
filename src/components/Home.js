8cc004b6442c48072f3eca00ac89fc35e33e0add
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-19 16:03:37 -0700

    Bold links on hover and rounded h1 box on langing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-
-=======
 import "./Home.css"
 
-export default function Home() {
->>>>>>> 57e43f2998231a9d4e27ec963134289ad0c43163
 
 export default function Home() {
 
     return (
-<<<<<<< HEAD
-        <>
-            <h1>Welcome to the Watering Hole!</h1>
-            <img src="./flower.jpg" alt="flowering plant"/>
-        </>
-=======
         <div className='home-container'>
             <h1>Welcome to the Watering Hole!</h1>
             {/* <img src="https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="flowering plant"/> */}
         </div>
->>>>>>> 57e43f2998231a9d4e27ec963134289ad0c43163
     )
 }

66f84063efd4b43ae46ce2bd8f66f39756f1dcbb
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-19 15:40:18 -0700

    reset

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,105 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
-import axios from 'axios';
-=======
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useHistory } from 'react-router';
-
-const initialValues = {
-    username: "",
-    password:"",
-    phonenumber: ""
-};
 
 
-export default function Login() {
-    // const { push } = useHistory();
-    const [formValues, setFormValues] = useState(initialValues);
-
-    const handleChange = (e) => {
-        setFormValues({
-            ...formValues,
-            [e.target.name]: e.target.value
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post('https://watermyplantsweb46.herokuapp.com/auth/login', formValues)
-        .then((res) => {
-            window.localStorage.setItem('token', res.data.payload);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-        .finally(() => {
-            setFormValues(initialValues)
-        })
-    };
->>>>>>> a03fe8610642afe491edd9a91f20d943e2263457
-
-export default function Login() {
-    
-
-<<<<<<< HEAD
-    return (
-        <>
-            <form id="login-form">
-=======
+export default function Home() {
 
     return (
         <>
-            <form id="login-form" onSubmit={handleSubmit}>
->>>>>>> a03fe8610642afe491edd9a91f20d943e2263457
-                <h1>Login to view your plants.</h1>
-                <p>Don't have a login? Create one!</p>
-
-                <div className='form-inputs' id='login-inputs'>
-                    <input
-<<<<<<< HEAD
-                        name="username"
-                        type="text"
-                        placeholder="username"
-                    />
-
-                    <input
-                        name="username"
-                        type="password"
-                        placeholder="password"
-=======
-                        value={formValues.username}
-                        name="username"
-                        type="text"
-                        placeholder="username"
-                        onChange={handleChange}
-                    />
-
-                    <input
-                        value={formValues.password}
-                        name="password"
-                        type="password"
-                        placeholder="password"
-                        onChange={handleChange}
-                    />
-
-                    <input
-                        value={formValues.phonenumber}
-                        name="phonenumber"
-                        type="tel"
-                        placeholder="phone number"
-                        onChange={handleChange}
->>>>>>> a03fe8610642afe491edd9a91f20d943e2263457
-                    />
-                </div>
-
-                <div className="errors">
-
-                </div>
-
-                <button id='login'>Login</button>
-
-            </form>
+            <h1>Welcome to the Watering Hole!</h1>
+            <img src="./flower.jpg" alt="flowering plant"/>
         </>
     )
 }

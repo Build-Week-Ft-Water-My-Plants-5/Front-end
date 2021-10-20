76fcacaa8a302316cb13d3f6054bfe626ed7d268
VABIII <plybyvern6@aol.com>
2021-10-19 19:46:03 -0500

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,8 @@ export default function Home() {
 
     return (
         <>
-            <h1>Welcome to the Watering Hole!</h1>
-            <img src="./flower.jpg" alt="flowering plant"/>
-
             <div className='home-container'>
                 <h1>Welcome to the Watering Hole!</h1>
-                {/* <img src="https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="flowering plant"/> */}
             </div>
         </>
     )

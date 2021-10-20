ba3c36f7aab56d5751c22745dd138bc943ccb45b
kennedybryant <kennedy_marisa96@yahoo.com>
2021-10-20 12:16:00 -0400

    Axios Auth

--- src/components/Sign_Up.js
+++ src/components/Sign_Up.js
@@ -28,14 +28,13 @@ export default function Signup() {
         e.preventDefault();
         axios.post('https://watermyplantsweb46.herokuapp.com/api/auth/register', formValues)
         .then((res) => {
-            window.localStorage.setItem('token', res.data.payload);
             push('./login');
         })
         .catch(err => {
             console.log(err.message);
         })
         .finally(() => {
-            setFormValues(formValues)
+            setFormValues(formValues);
             console.log(formValues);
         })
     };
@@ -43,40 +42,46 @@ export default function Signup() {
 
     return (
         <>
-            <form id='signup-form' onSubmit={handleSubmit}>
-                <h1>Create an account</h1>
-                <p>Already have an account? <Link id ="login" to="/login">Login here!</Link></p>
-
-                <div className="form-inputs" id="signup-inputs">
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            value={formValues.username}
-                            id="username"
-                            name="username"
-                            type="text"
-                            onChange={handleChange}
-                        />
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            value={formValues.password}
-                            id="password"
-                            name="password"
-                            type="password"
-                            onChange={handleChange}
-                        />
-                        <label htmlFor="phonenumber">Phone Number:</label>
-                        <input
-                            value={formValues.phonenumber}
-                            id="phonenumber"
-                            name="phonenumber"
-                            type="tel"
-                            onChange={handleChange}
-                            />
-                    <div className="errors">
+            <div className="main-container">
+                <form id='signup-form' onSubmit={handleSubmit}>
+                    <div className="form-header">
+                        <h1>Create an account</h1>
+                        <p>Already have an account? <Link id ="login" to="/login">Login here!</Link></p>
                     </div>
-                </div>
-                <button id='signup'>Sign Up</button>
-            </form>
+
+                    <div className="input-container">
+                        <div className="form-inputs" id="signup-inputs">
+                                <label htmlFor="username">Username:</label>
+                                <input
+                                    value={formValues.username}
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    onChange={handleChange}
+                                />
+                                <label htmlFor="password">Password:</label>
+                                <input
+                                    value={formValues.password}
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    onChange={handleChange}
+                                />
+                                <label htmlFor="phonenumber">Phone Number:</label>
+                                <input
+                                    value={formValues.phonenumber}
+                                    id="phonenumber"
+                                    name="phonenumber"
+                                    type="tel"
+                                    onChange={handleChange}
+                                    />
+                            <div className="errors">
+                            </div>
+                        </div>
+                        <button id='signup-button'>Sign Up</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }

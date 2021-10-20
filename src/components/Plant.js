545c3efe8eb8e2be2cf3dcdb89922485ccc47ac0
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-20 12:06:46 -0700

    Delete function... still needs to route to correct page

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -7,9 +7,10 @@ export default function Plant(props) {
     const {plant, set_plant_values} = props;
 
 
-    const delete_plant = (existing_plant) => {
-        console.log(existing_plant.id)
-        axiosWithAuth().delete(`https://watermyplantsweb46.herokuapp.com/api/plants/${existing_plant.id}`, existing_plant)
+    const delete_plant = (e) => {
+        e.preventDefault();
+        console.log(plant)
+        axiosWithAuth().delete(`https://watermyplantsweb46.herokuapp.com/api/plants/${plant.plants_id}`, plant)
             .then(res => {
               push('/PlantList');
             })
@@ -39,17 +40,15 @@ export default function Plant(props) {
 
     return (
         <>
-            <div className="plant-card">
+            <form className="plant-card">
                 <h3>Plant Info</h3>
                 <p>{plant.nickname}</p>
                 <p>{plant.species}</p>
                 <p>{plant.h2oFrequency}</p>
                 <img src={`${plant.image}`} alt={`Image of a ${plant.nickname}.`}/>
-                
                 <button onChange={update_form}>Edit Plant</button>
-                <button onChange={delete_plant}>Delete Plant</button>
-                
-            </div>
+                <button onClick={delete_plant}>Delete Plant</button>
+            </form>
         </>
     )
 
